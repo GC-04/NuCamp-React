939ec2085aebb71fac96fa0d0836501fac60b4ab
GC-04 <97651069+GC-04@users.noreply.github.com>
2022-03-11 00:43:17 -0700

    footer header router

---
+++ src/components/MainComponent.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Directory from './DirectoryComponent';
+import CampsiteInfoComponent from './CampsiteInfoComponent';
+import Header from './Header Component';
+import Footer from './Footer Component';
+import Home from './Home Component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import {CAMPSITES} from '../shared/campsites'
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES,
+
+        };
+    }
+
+
+    render() {
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        }
+
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage}/>
+                    <Route exact path='/directory' render={() =>   <Directory campsites={this.state.campsites}/>} />
+                    <Redirect to='/home' />
+                </Switch>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Main;
